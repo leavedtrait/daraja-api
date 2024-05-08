ed49a370b4ff85c10712c43feb4f935134b24fca
leavedtrait <leavedtrait@gmail.com>
2024-05-09 01:29:41 +0300

    to ureq instead of surf

--- src/common/utils.rs
+++ src/common/utils.rs
@@ -1,23 +1,46 @@
-use surf::{get,client,Error};
-use base64::encode;
+//use surf::{get,client,Error};
+use base64::{self, engine::general_purpose, Engine};
 
 use crate::models::models::AuthToken;
+use ureq::{get, Agent, Error};
 
-pub async fn get_mpesa_token() -> Result<AuthToken, Error> {
+// Helper function to encode the credentials (replace with your actual encoding logic)
+fn encode(data: &str) -> String {
+    
+    general_purpose::STANDARD.encode(data)
+    //base64::encode(data)
+}
+
+
+pub async fn get_mpesa_token() -> Result<AuthToken, ureq::Error> {
     let url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
     let auth_header = format!(
         "Basic {}",
         encode("oB3k4GMt4KZR9o6Msob3Qne3oDwAfxbk:XxohAhWngRijrrh3")
     );
+    let auth_token : AuthToken;
 
-    let client = surf::client();
-    let req  = get(url).header("Authorization", auth_header);
-    let mut res = client.send(req).await?;
- 
-    
+    let res = get(url)
+        .set("Authorization", &auth_header)  // Set the Authorization header
+        .call();
 
+    match res {
+        Ok(res) => {
+            /* it worked */
+            auth_token = res.into_json()?;
+            Ok(auth_token)
+        },
+        Err(ureq::Error::Status(code, response)) => {
+            response.status();
+            /* the server returned an unexpected status
+               code (such as 400, 500 etc) */
+            Err(Error::Status(code, response))
+        }
+        Err(ureq::Error::Transport(transport)) => { 
+            /* some kind of io/transport error */
+            Err(Error::Transport(transport))
+         }
 
-    let auth_token: AuthToken = res.body_json().await?;
-
-    Ok(auth_token)
+    }
 }
+
--- src/handlers/handlers.rs
+++ src/handlers/handlers.rs
@@ -5,8 +5,8 @@ use tide::prelude::*;
 
 pub async fn get_token(mut req: Request<()>) -> tide::Result {
     let res = utils::get_mpesa_token().await?;
+
     let response = serde_json::to_string(&res)?;
 
     Ok(response.into())
-
 }
